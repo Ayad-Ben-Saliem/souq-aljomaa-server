473b3431dca10c286e3afca2bd023a1684777244
Ayad Ben Saliem <ayad.bslm@gmail.com>
2024-07-19 22:24:47 +0200

    Edit service.bat (Use current work directory instead of static absolute path)

--- service.bat
+++ service.bat
@@ -1,6 +1,8 @@
 @echo off
 
-nssm-2.24\win64\nssm.exe install "Souq Aljomaa Service" C:\Users\HP\VSCodeProjects\souq-aljomaa-server\start_flask_app.bat
-nssm-2.24\win64\nssm.exe set "Souq Aljomaa Service" AppStdout C:\Users\HP\VSCodeProjects\souq-aljomaa-server\service_output.log
-nssm-2.24\win64\nssm.exe set "Souq Aljomaa Service" AppStderr C:\Users\HP\VSCodeProjects\souq-aljomaa-server\service_error.log
+set CURRENT_DIR=%~dp0
+
+nssm-2.24\win64\nssm.exe install "Souq Aljomaa Service" "%CURRENT_DIR%start_flask_app.bat"
+nssm-2.24\win64\nssm.exe set "Souq Aljomaa Service" AppStdout "%CURRENT_DIR%service_output.log"
+nssm-2.24\win64\nssm.exe set "Souq Aljomaa Service" AppStderr "%CURRENT_DIR%service_error.log"
 nssm-2.24\win64\nssm.exe start "Souq Aljomaa Service"
